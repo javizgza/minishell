fca1374bfd25900d245c9966b16bf0644a53adc9
Javier Zaragoza Tejeda <javierzaragozatejeda@MacBook-Pro-de-Javier.local>
2025-02-11 12:47:24 +0100

    Update

--- src/parsing/handle_redirections.c
+++ src/parsing/handle_redirections.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_redirections.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jazarago <jazarago@student.42.fr>          +#+  +:+       +#+        */
+/*   By: javierzaragozatejeda <javierzaragozatej    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 17:01:47 by codespace         #+#    #+#             */
-/*   Updated: 2025/01/30 13:45:32 by jazarago         ###   ########.fr       */
+/*   Updated: 2025/02/05 11:46:05 by javierzarag      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ void    parse_redir(char *input)
     actual_command = malloc(sizeof(t_commands));
     if (!actual_command)
         return;
-    memset(actual_command, 0, sizeof(t_commands)); // Initialize the structure
-    actual_command->args = malloc(sizeof(char *) * 1024); // Allocate memory for args
+    memset(actual_command, 0, sizeof(t_commands));
+    actual_command->args = malloc(sizeof(char *) * 1024);
     if (!actual_command->args)
     {
         free(actual_command);
@@ -39,7 +39,16 @@ void    parse_redir(char *input)
     }
     while (tokens[i].type != END)
     {
-        ft_set_up_redir(actual_command, tokens[i].type, &tokens[i].value);
+        if (tokens[i].type == BIGGER || tokens[i].type == BIGGERX2
+            || tokens[i].type == SMALLER || tokens[i].type == SMALLERX2)
+        {
+            t_tokens redir_type = tokens[i].type;
+            i++;
+            if (tokens[i].type != END)
+                ft_set_up_redir(actual_command, redir_type, &tokens[i].value);
+        }
+        else
+            ft_set_up_redir(actual_command, tokens[i].type, &tokens[i].value);
         i++;
     }
     actual_command->args[actual_command->args_num] = NULL;
