1c753d2623a68877af8bf35ed6d4edf211bb2d7e
javizgza <javierzaragoza144@gmail.com>
2025-03-13 14:11:59 +0100

    Changed Unset signals

--- src/parsing/signals.c
+++ src/parsing/signals.c
@@ -6,7 +6,7 @@
 /*   By: jazarago <jazarago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 11:36:45 by jazarago          #+#    #+#             */
-/*   Updated: 2025/03/13 14:03:57 by jazarago         ###   ########.fr       */
+/*   Updated: 2025/03/13 14:11:18 by jazarago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	ft_new_line(int signal)
 	printf("\n");
 }
 
-void	unset_signal(void)
+void	unset_signals(void)
 {
 	signal(SIGINT, ft_new_line);
 }
